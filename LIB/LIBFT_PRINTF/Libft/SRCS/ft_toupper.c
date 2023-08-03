d8f7ba13a997c5e5fac83d293a8402c725a23487
alissiafrolova <frolova.alissia@gmail.com>
2023-08-03 23:58:12 +0200

    final

--- LIB/LIBFT_PRINTF/Libft/SRCS/ft_toupper.c
+++ LIB/LIBFT_PRINTF/Libft/SRCS/ft_toupper.c
@@ -6,21 +6,18 @@
 /*   By: afrolova <afrolova@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:33:11 by afrolova          #+#    #+#             */
-/*   Updated: 2022/05/12 15:31:42 by afrolova         ###   ########.fr       */
+/*   Updated: 2023/06/06 22:56:15 by alissiafrolov    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 int	ft_toupper(int c)
 {
-	unsigned char	a;
-
 	if (c < 0)
 		return (-1);
-	a = (unsigned char)c;
 	if (c >= 'a' && c <= 'z')
 	{
 		c = c - 32;
 	}
-	return ((int)c);
+	return (c);
 }
